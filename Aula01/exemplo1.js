8cd74486dfb807ea748d25b8d37418135cea7762
RodrigoRochaBispoSilva <rodrigorochabispo@gmail.com>
2026-02-24 11:37:59 -0300

    24/2/26

---
+++ Aula01/exemplo1.js
@@ -0,0 +1,18 @@
+document.write ("<h2>Entrada de Dados</h2>");
+
+var numero1 = prompt("Digite o numero 1"); 
+var numero2 = prompt("Digite o numero 2");
+
+document.write("Numero 1 =" + numero1 + "<br>");
+document.write("Numero 2 =" + numero2 + "<br>");
+
+
+document.write ("<h2>Operacoes Aritmeticas</h2>");
+var resultado;
+
+
+resultado = Number(numero1) + Number(numero2);
+document.write("A soma dos numeros e " + resultado + "<br>");
+
+resultado = Number(numero1) - Number(numero2);
+document.write("A diferenca entre os numeros e" + resultado + "<br>");
---
+++ Aula01/script.js
@@ -0,0 +1,17 @@
+var nome = "Rodrigo";
+var idade = 16;
+var altura = 1.75;
+
+console.log( nome );
+console.log( idade ); 
+console.log( altura );
+
+document.write("<h1>Ola, seja bem vindo ao JavaScript</h1>")
+
+document.write( nome + "<br>" );
+document.write( idade + "<br>" );
+document.write( altura + "<br>" );
+
+var dias = idade * 365;
+
+document.write("<p style='color : red'>Voce ja viveu " + dias + "") 
