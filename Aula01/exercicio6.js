5d3afe1889928f36786c68244442371ab921824b
RodrigoRochaBispoSilva <rodrigorochabispo@gmail.com>
2026-03-17 11:39:33 -0300

    17/3

---
+++ Aula01/exercicio6.js
@@ -0,0 +1,9 @@
+var quantidade = Number (prompt("Digite a quantidade do produto:"));
+var preco = Number (prompt("Digite a preco do produto:"));
+
+
+
+subtotal= quantidade * preco
+desconto = subtotal * 0.10
+final = subtotal - desconto 
+
