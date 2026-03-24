0cc6696d31e11b20d43b41f3c1c943eabd150973
RodrigoRochaBispoSilva <rodrigorochabispo@gmail.com>
2026-03-24 11:38:03 -0300

    24/3

---
+++ Aula02/exercicio2.js
@@ -0,0 +1,9 @@
+document.write("<h1>Aula 02 - Exercicio 2</h1>");
+
+var fah = prompt("Digite a temperatura em fahrenheit:");
+
+var tcelsius = ((Number(fahrenheit) - 32) * 5) / 9
+
+document.write("A temperatura em celsius e: " + tcelsius)
+
+document.getElementById("resultado").innerHTML = "<p>O quadrado do numero e " + quadrado + " e o cubo e " + cubo + "</p>";
